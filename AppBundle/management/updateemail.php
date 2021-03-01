2980a7aa67cfd8647fd4e96854e7e8a8531c2f3e
Zachlaf <zachary.lafreniere@hotmail.ca>
2021-03-01 10:55:33 -0500

    commit pour mich

--- AppBundle/management/updateemail.php
+++ AppBundle/management/updateemail.php
@@ -1,8 +1,9 @@
 <?php
-require_once (__DIR__."/DB/Model/ModelAccounts.php");
+require_once(__DIR__."/DB/Model/ModelAccounts.php");
 require_once(__DIR__ . "/DB/Entity/EntityAccounts.php");
 $modelAccounts = new ModelAccounts();
 $entityAccount = new EntityAccounts();
+
 if (!empty($_POST["oldEmail"]) && !empty($_POST["email"])) {
     $oldEmail = $_POST["oldEmail"];
     $email = $_POST["email"];
@@ -10,6 +11,7 @@ if (!empty($_POST["oldEmail"]) && !empty($_POST["email"])) {
         if (!$entityAccount->checkAccountEmailUsed($email)) {
             $result = $modelAccounts->updateAccountEmail($email, $oldEmail);
             if ($result == "ok") {
+
                 echo json_encode(array("item" => $result));
             } else {
                 echo json_encode(array("item" => "error"));
